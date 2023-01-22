2d80c2e4da00d69cc9137a80d74bf86bccd5b082
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-01-22 17:38:50 -0500

    added tests for segment node

--- test/input/components/segment/SegmentNodeTest.java
+++ test/input/components/segment/SegmentNodeTest.java
@@ -2,8 +2,76 @@ package input.components.segment;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
+
+import input.components.point.PointNode;
 
 public class SegmentNodeTest {
 
+	@Test
+	void segmentNodeTest() {
+		PointNode point1 = new PointNode(0,0);
+		PointNode point2 = new PointNode(1,1);
+		SegmentNode segment = new SegmentNode(point1, point2);
+		
+		assertEquals(point1, segment.getPoint1());
+		assertEquals(point2, segment.getPoint2());
+	}
+	
+	@Test
+	void segmentNodeNullPointsTest() {
+		SegmentNode segment = new SegmentNode(null, null);
+		
+		assertEquals(null, segment.getPoint1());
+		assertEquals(null, segment.getPoint2());
+	}
+	
+	@Test
+	void equalsNotEqual() {
+		SegmentNode segment1 = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		SegmentNode segment2 = new SegmentNode(new PointNode(2,2), new PointNode(3,3));
+		
+		assertEquals(false, segment1.equals(segment2));
+	}
+	
+	@Test
+	void equalsSame() {
+		SegmentNode segment1 = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		SegmentNode segment2 = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		
+		assertEquals(true, segment1.equals(segment2));
+	}
+	
+	@Test
+	void equalsFlipped() {
+		SegmentNode segment1 = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		SegmentNode segment2 = new SegmentNode(new PointNode(1,1), new PointNode(0,0));
+		
+		assertEquals(true, segment1.equals(segment2));
+	}
+	
+	@Test
+	void equalsOtherType() {
+		SegmentNode segment = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		assertEquals(false, segment.equals(map));
+	}
+	
+	@Test
+	void equalsSelf() {
+		SegmentNode segment = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		
+		assertEquals(true, segment.equals(segment));
+	}
+	
+	@Test
+	void equalsNull() {
+		SegmentNode segment1 = new SegmentNode(new PointNode(0,0), new PointNode(1,1));
+		SegmentNode segment2 = null;
+		
+		assertEquals(false, segment1.equals(segment2));
+	}
 }
