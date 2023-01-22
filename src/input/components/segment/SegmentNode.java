56b0128b88cf07787d5676fe3c1789d4fe01bf82
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-01-22 15:19:05 -0500

    added equals method to segmentNode

--- src/input/components/segment/SegmentNode.java
+++ src/input/components/segment/SegmentNode.java
@@ -1,6 +1,9 @@
 package input.components.segment;
 
+import java.util.Objects;
+
 import input.components.point.PointNode;
+import utilities.math.MathUtilities;
 
 /**
  * A utility class only for representing ONE segment
@@ -17,10 +20,22 @@ public class SegmentNode
 		_point1 = pt1;
 		_point2 = pt2;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
-		// TODO
-		return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SegmentNode other = (SegmentNode) obj;
+		return MathUtilities.doubleEquals(_point1.getX(), other._point1.getX()) &&
+			   MathUtilities.doubleEquals(_point1.getY(), other._point1.getY()) &&
+			   MathUtilities.doubleEquals(_point2.getX(), other._point2.getX()) &&
+			   MathUtilities.doubleEquals(_point2.getY(), other._point2.getY());
 	}
+
+	
+	
 }
