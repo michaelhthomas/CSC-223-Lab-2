b169f53097c0e9fa02e72e5d3398fb1e666309f6
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-01-22 16:08:15 -0500

    added functions to segmentNodeDatabase
    constructers, add undirected edge, add directed edge, count undirected edges, and add adjacency list

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -2,6 +2,8 @@ package input.components.segment;
 
 import input.components.point.PointNode;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,23 +12,39 @@ public class SegmentNodeDatabase {
 
 	Map<PointNode, Set<PointNode>> _adjLists;
 	
-	public SegmentNodeDatabase() {	
+	public SegmentNodeDatabase() {
+		_adjLists = new HashMap<PointNode, Set<PointNode>>();
 	}
 	
-	public void SegmntNodeDatabase(Map<PointNode, Set<PointNode>> adjList) {
+	public void SegmntNodeDatabase(Map<PointNode, Set<PointNode>> adjLists) {
+		_adjLists = adjLists;
 	}
 	
 	public int numUndirectedEdges() {
-		return 0;
+		return _adjLists.size();
 	}
 	
 	public void addDirectedEdge(PointNode point1, PointNode point2) {
+		Set<PointNode> connectedPoints = _adjLists.get(point1);
+		if(connectedPoints == null) {
+			LinkedHashSet<PointNode> newPoint = new LinkedHashSet<PointNode>();
+			newPoint.add(point2);
+			_adjLists.put(point1, newPoint);
+		}
+		else {
+			connectedPoints.add(point2);
+		}
 	}
 	
 	public void addUndirectedEdge(PointNode point1, PointNode point2) {
+		addDirectedEdge(point1, point2);
+		addDirectedEdge(point2, point1);
 	}
 	
-	public void addAdjacencyList(PointNode point, List<PointNode>adjPoints) {
+	public void addAdjacencyList(PointNode point, List<PointNode> adjPoints) {
+		for(PointNode connectedPoint : adjPoints) {
+			addUndirectedEdge(point, connectedPoint);
+		}
 	}
 	
 	public List<SegmentNode> asSegmentList() {
